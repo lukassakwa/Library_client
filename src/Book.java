ea54f5396b6dcdca23408021513f0308d97c880e
oliwier <oliviersakwa@gmail.com>
2021-11-21 18:21:54 +0100

    initial commit

--- src/Book.java
+++ src/Book.java
@@ -1,24 +1,17 @@
 import java.util.Objects;
 
 public class Book {
-    //The class that represents a single book
-    //Contains the following variables:
-    //-title as String
-    //-author as String
-    //-pages as int
 
     private String title;
     private String author;
     private int pages;
 
-    //Constructor
     Book(String title, String author, int pages){
         this.title = title;
         this.author = author;
         this.pages = pages;
     }
 
-    //Print a single book on the screen
     public void readBook(){
         System.out.println("Title: "+title+" / Author: "+author+" / Pages: "+pages);
     }
